db7210a2713961e5fb8e5e6113d760364399fd07
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-23 00:13:20 -0300

    Create ICriptografia.cs

---
+++ src/Interfaces/ICriptografia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace Colaboracao.Core
+{
+    public interface ICriptografia
+    {
+        string Encrypt(string plainText);
+
+        string Decrypt(string encryptedText);
+
+        string MD5Hash(string text);
+
+    }
+}
