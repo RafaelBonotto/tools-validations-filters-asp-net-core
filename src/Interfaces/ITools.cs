24c682d644935a1b6f7392bf0c68d23b1e3957f9
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-23 00:12:51 -0300

    Create ITools.cs

---
+++ src/Interfaces/ITools.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.Drawing;
+
+namespace Colaboracao.Core
+{
+    public interface ITools
+    {
+        long jsonStringToLong(string numero);
+
+        bool verificaArquivo(string arquivo);
+
+        bool gravaImagem(string caminho, byte[] imagem, string nomeImagem);
+
+        bool gravaImagemThumb(string caminho, byte[] imagem, string nomeImagem);
+        bool gravaImagemThumbMini(string caminho, byte[] imagem, string nomeImagem);
+
+        bool gravaImagemThumbVeryMini(string caminho, byte[] imagem, string nomeImagem);
+
+        Image ResizeImage(Image imgToResize, Size destinationSize);
+
+        bool gravaArquivo(string caminho, byte[] arquivo, string nomeArquivo);
+
+        bool deletaImagem(string caminho, string nomeImagem);
+
+        bool validaCnpj(string cnpj);
+
+        bool validaCpf(string cpf);
+
+        bool validaEmail(string email);
+    }
+}
