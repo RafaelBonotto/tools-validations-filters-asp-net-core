4b25c90c8678630c40e4df34ac8669ed2b78d5ca
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-23 00:12:11 -0300

    Create Tokens.cs

---
+++ src/Impl/Tokens.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Colaboracao.Core.Interfaces;
+
+namespace Colaboracao.Core.Impl
+{
+    public class Tokens : ITokens
+    {
+        public string ReverterToken(string token)
+        {
+            try
+            {
+                token = token.Split(':')[0];
+                return Criptografia.Decrypt(token).Split('|')[0];
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public string Base64(string token)
+        {
+            try
+            {
+                byte[] textoAsBytes = Encoding.ASCII.GetBytes(token);
+                return System.Convert.ToBase64String(textoAsBytes);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
