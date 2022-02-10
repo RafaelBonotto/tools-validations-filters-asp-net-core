f8d4672b5416b940dfa272dcac54fb0a60a69c6c
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-02-09 23:31:40 -0300

    Create ILogCore.cs

---
+++ src/Interfaces/ILogCore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Colaboracao.Core
+{
+    public interface ILogCore
+    {
+        void Log(string message, Levels level);
+    }
+}
