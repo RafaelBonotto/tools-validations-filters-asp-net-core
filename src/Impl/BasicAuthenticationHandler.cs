853a676746ab2fec92a1ccc9e4bb0da88b1c76b4
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-02-09 23:31:12 -0300

    Create BasicAuthenticationHandler.cs

---
+++ src/Impl/BasicAuthenticationHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using ApiClient.Domain.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Colaboracao.Core.Impl
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly IUsuarioClient _usuarioClient;
+
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock,
+            IUsuarioClient usuarioClient)
+            : base(options, logger, encoder, clock)
+        {
+            _usuarioClient = usuarioClient;
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+
+            string usuario = null;
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var token = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Parameter));
+
+                if (!await _usuarioClient.ValidarToken(token))
+                {
+                    return AuthenticateResult.Fail("Token Invalido");
+                }
+                else
+                {
+                    usuario = token;
+                }
+            }
+            catch (Exception ex)
+            {
+                return AuthenticateResult.Fail("Invalid Authorization Header");
+            }
+
+            var claims = new[] {
+                new Claim("Token", usuario)
+            };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+    }
+}
